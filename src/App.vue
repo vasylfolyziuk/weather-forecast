067d8bf0c67c8e4478600a6654f54590d4c65adf
Vasyl Folyziuk <vasylfolyziuk@MacBook-Pro-Vasyl.local>
2023-11-22 16:32:53 +0200

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Header from '@/components/Header/Header.vue';
+import NavBar from '@/components/NavBar/NavBar.vue';
 </script>
 
 <template>
   <Header />
   <div class="wrapper">
-    <nav>
-      <RouterLink to="/" activeClass="active">Home</RouterLink>
-      <RouterLink to="/favorites" activeClass="active">About</RouterLink>
-    </nav>
+    <NavBar />
   </div>
 
   <RouterView />
 </template>
-
-<style scoped>
-  .active {
-    color: red;
-  }
-</style>
 
---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="d-flex">
+    <RouterLink
+      to="/"
+      class="nav-item"
+      activeClass="active">
+      Home
+    </RouterLink>
+    <RouterLink 
+      to="/favorites" 
+      class="nav-item"
+      activeClass="active">
+      About
+    </RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+  .nav-item {
+    display: block;
+    padding: .5rem 1rem;
+    border: 1px solid grey;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    margin-right: 0.5rem;
+    color: var(--blue);
+  }
+  .nav-item:hover {
+    color: var(--nav-item-hover);
+  }
+  .active {
+    color: var(--nav-item-active);
+  }
+</style>
